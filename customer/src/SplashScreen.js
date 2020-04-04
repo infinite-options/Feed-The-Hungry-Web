a0337c19af26f71972f0df3cb04a7ea8d0f9c3d5
Zach <zlind@ucsc.edu>
2020-04-03 20:31:23 -0700

    updated splash screen with styling

--- customer/src/SplashScreen.js
+++ customer/src/SplashScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import {Jumbotron, Button} from 'react-bootstrap';
+import {Jumbotron, Button, Container, Row} from 'react-bootstrap';
+
+import './SplashScreen.css';
 
 function SplashScreen(props) {
-    return (
-        <Jumbotron>
-            {/* todo: prettify, link to sign up */}
-            <h1>Welcome to Serving Now</h1>
-            <p>Helping the hungry find the food they need</p>
+  return (
+    <Jumbotron>
+      <div className='filter text-white'>
+        <Container className='d-flex flex-column text-center'>
+          {/* todo: prettify, link to sign up */}
+          <h1 className='display-1 font-weight-bold'>Serving Now</h1>
+          <p className='h3'>Helping the hungry find the food they need</p>
+          <Row className='align-self-center mt-3 d-flex justify-content-between'>
             <Button onClick={props.login} className='mx-3'>Log in</Button>
             <Button onClick={props.signup} className='mx-3'>Sign up</Button>
-        </Jumbotron>
-    );
+          </Row>
+        </Container>
+      </div>
+    </Jumbotron>
+  );
 }
 
 export default SplashScreen;
