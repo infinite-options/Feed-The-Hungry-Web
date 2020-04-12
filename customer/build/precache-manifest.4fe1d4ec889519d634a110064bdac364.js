eb943b6632b13602576c9057eeb1d53272489b06
Zach <zlind@ucsc.edu>
2020-04-11 18:02:14 -0700

    built react app and api into same build directory

---
+++ customer/build/precache-manifest.4fe1d4ec889519d634a110064bdac364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7974f9f4c47c6a243e7853c142b251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "634216c306e0fdaec953",
+    "url": "/static/css/main.b06e6579.chunk.css"
+  },
+  {
+    "revision": "14c054e1aa874d0fdc06",
+    "url": "/static/js/2.b554e643.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b554e643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634216c306e0fdaec953",
+    "url": "/static/js/main.d28beda8.chunk.js"
+  },
+  {
+    "revision": "e1e5115e15c8341394c0",
+    "url": "/static/js/runtime-main.f5fa7a29.js"
+  },
+  {
+    "revision": "7128b6c528ef07d328ac3cb64aeedb1b",
+    "url": "/static/media/background.7128b6c5.jpg"
+  }
+]);
